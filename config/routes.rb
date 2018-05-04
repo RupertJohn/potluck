72cef26264f13b77a3348a908754e1f344af2474
Rupert Saxton <rsaxton@covermymeds.com>
2018-05-04 09:58:43 -0400

    Create API endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   root 'default#index'
+
+  defaults format: :json do
+    namespace :api do
+      namespace :v1 do
+        resource :potluck
+        resources :dish
+      end
+    end
+  end
 end
